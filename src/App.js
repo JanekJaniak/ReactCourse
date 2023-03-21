675f97e73801d8d967e31efc48ddedc656827946
Janek Janiak <janek.janiak@gmail.com>
2023-03-21 20:32:27 +0100

    add initialExp

--- src/App.js
+++ src/App.js
@@ -4,15 +4,28 @@ import Expenses from './components/Expenses/Expenses';
 import { NewExpense } from './components/NewExpense/NewExpense';
 
 const App = () => {
-  const firstExpense =
+  const initialExpenses = [
     {
       id: 'e1',
       title: 'Toilet Paper',
       price: 94.12,
+      date: new Date(2023, 2, 22),
+    },
+    {
+      id: 'e2',
+      title: 'Bike',
+      price: 9994.12,
       date: new Date(2020, 7, 14),
-    };
+    },
+    {
+      id: 'e3',
+      title: 'car',
+      price: 9994.12,
+      date: new Date(2023, 8, 14),
+    },
+  ]
 
-  const [expenses, setExpenses] = useState([firstExpense])
+  const [expenses, setExpenses] = useState(initialExpenses)
 
   // return React.createElement(
   //   'div',
